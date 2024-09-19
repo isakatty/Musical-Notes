fd0cba40a2c0672e4f93e6eb560e11842ebd15e9
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-09-19 12:45:19 +0900

    [Setting] 프로젝트 추가

---
+++ Musical-Notes/Musical_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Musical_NotesApp.swift
+//  Musical-Notes
+//
+//  Created by Jisoo Ham on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Musical_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
