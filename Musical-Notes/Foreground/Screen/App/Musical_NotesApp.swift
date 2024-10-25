5961dbe2bc84d33362c79ed87864645af0210010
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-10-25 17:40:56 +0900

    [Feat] Remote Push notification 기능 추가

--- Musical-Notes/Foreground/Screen/App/Musical_NotesApp.swift
+++ Musical-Notes/Foreground/Screen/App/Musical_NotesApp.swift
@@ -7,11 +7,85 @@
 
 import SwiftUI
 
+import Firebase
+import FirebaseMessaging
+
 @main
 struct Musical_NotesApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    let gcmMessageIDKey = "gcm.message_id"
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        
+        // 원격 알림
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOption: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOption,
+            completionHandler: {_, _ in }
+        )
+        
+        application.registerForRemoteNotifications()
+        
+        // 메세지 대리자
+        Messaging.messaging().delegate = self
+        
+        return true
+    }
+}
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // APNS 등록 성공 시 호출되는 메서드
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // APNS 토큰을 Firebase에 전달
+        Messaging.messaging().apnsToken = deviceToken
+        print("APNS token registered: \(deviceToken)")
+    }
+    
+    // APNS 등록 실패 시 호출되는 메서드
+    func application(_ application: UIApplication,
+                     didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register for remote notifications: \(error.localizedDescription)")
+    }
+}
+// FCM
+extension AppDelegate: MessagingDelegate {
+    // 현재 등록 토큰 가져오기(device마다의 고유한 토큰 정보)
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        
+        // 현재 등록 토큰 가져오기
+        Messaging.messaging().token { token, error in
+            if let error = error {
+                print("Error fetching FCM registration token: \(error)")
+            } else if let token = token {
+                print("FCM registration token: \(token)")
+            }
+        }
+        
+        // 토큰 모니터링 코드 - 변경되면
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+    }
+}
+
